dc870ef25e0be66b6762108d250f5fd5cc47697e
Carson <carsoncolgateccu@gmail.com>
2021-09-15 17:38:50 -0600

    added more prompts for criteria

--- script.js
+++ script.js
@@ -6,7 +6,42 @@ var num = "0123456789";
 var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 var passlength="";
 
+var lowerconfirm=[];
+var upperconfirm=[];
+var numconfirm=[];
+var specialcharconfirm=[];
+
+
+
 // Write password to the #password input
+function generatePassword(){
+  
+  function criteria() {
+  passlength = prompt("How many Characters would you like in your password?");{
+    if (passlength<8 || passlength >128) {
+      alert("password does not match criteria min 8: max: 128");}
+      criteria();
+  }};
+  criteria();
+
+  function stuff(){
+    lowerconfirm= confirm("Do you want lower case characters?");
+    upperconfirm= confirm("Do you want upper case charracters");
+    numconfirm= confirm("Do you want numbers?");
+    specialconfirm= confirm("Do you want special characters?");
+    if(!lowercase && !uppercase && !num && !specialChar){
+      alert("Please choose one");
+      stuff();
+    }};
+    stuff();
+  }
+
+
+    
+
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -14,18 +49,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-  
-  
-
-  // Add event listener to generate button
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
-  passlength = prompt("How many Characters would you like in your password?");
-    if (passlength<8 || passlength >128) {
-      alert("password does not match criteria");
-      return generatePassword();
-      
-    }
-  }
+

4a3276283d0bacae85187d65a1781a772442f73f
Carson <carsoncolgateccu@gmail.com>
2021-09-15 23:47:53 -0600

    added new way to concat my arrays together using a variable and a function

--- script.js
+++ script.js
@@ -4,26 +4,30 @@ var upperCaseli = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 var lowerCaseli = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numli = [0,1,2,3,4,5,6,7,8,9,];
 var specialCharli =  ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^","~", "*", "?", ":"];
-// var passlength="";
-// var passwordarr= [];
+var passlength="";
+var passwordarr= [];
 
-// var lowerconfirm=[];
-// var upperconfirm=[];
-// var numconfirm=[];
-// var specialcharconfirm=[];
+var lowerconfirm=[];
+var upperconfirm=[];
+var numconfirm=[];
+var specialcharconfirm=[];
+
+var passwordarray = function();
+  if (upperconfirm) {
+    passswordarr = passwordarr.concat(upperCaseli);
+  }
+  if (lowerconfirm) {
+    passswordarr = passwordarr.concat(lowerCaseli);
+  }
+  if (numconfirm) {
+    passswordarr = passwordarr.concat(numli);
+  }
+  if (specialcharconfirm) {
+    passswordarr = passwordarr.concat(specialCharli);
+  }
 
 // Write password to the #password input
 function generatePassword(){
-// created all my variables for my functions
-  var array=[];
-  var char = "";
-  // these are still showing never read
-  var passLength;
-  var uppercase;
-  var lowercase;
-  var number;
-  var specialchar;
-// created first prompt asking telling how many characters my password will generate
   function criteria() {
   passlength = prompt("How many Characters would you like in your password?");{
     if (passLength<8 || passLength >128) {
@@ -32,30 +36,18 @@ function generatePassword(){
   }};
   criteria();
 // created folling confirms that should pop up and ok or cancel will add/omit to my pass
-  function stuff(){
+  function criteria(){
     lowercase= confirm("Do you want lower case characters?");
     uppercase= confirm("Do you want upper case charracters");
     number= confirm("Do you want numbers?");
     specialchar= confirm("Do you want special characters?");
     if(!lowercase && !uppercase && !number && !specialChar){
       alert("Please choose one");
-      stuff();
+      criteria();
     }};
-    stuff();
+    criteria();
   }
-// if confirmed this will add a random number,letter,specialchar to pass
-stuff();
-if (lowercase){ char += lowerCaseli};
-if (uppercase){char += upperCaseli};
-if (number){char += numli};
-if (specialchar){char += specialCharli};
-  // makes the choice random
-for (i=0;i<passlength;i++){
-  array[i] = char.charAt(Math.floor(Math.random()* char.length));
-}
-// makes pass a string
-array = array.join(" ");
-return array;
+
 
     
 function writePassword() {
@@ -67,3 +59,29 @@ function writePassword() {
 
 generateBtn.addEventListener("click", writePassword);
 }
+
+
+// created all my variables for my functions
+  // var array=[];
+  // var char = "";
+  // // these are still showing never read
+  // var passLength;
+  // var uppercase;
+  // var lowercase;
+  // var number;
+  // var specialchar;
+// created first prompt asking telling how many characters my password will generate
+
+// if confirmed this will add a random number,letter,specialchar to pass
+// stuff();
+// if (lowercase){ char += lowerCaseli};
+// if (uppercase){char += upperCaseli};
+// if (number){char += numli};
+// if (specialchar){char += specialCharli};
+//   // makes the choice random
+// for (i=0;i<passlength;i++){
+//   array[i] = char.charAt(Math.floor(Math.random()* char.length));
+// }
+// // makes pass a string
+// array = array.join(" ");
+// return array;

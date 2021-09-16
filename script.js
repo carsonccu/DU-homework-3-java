598b017ebc48452274f2651fca6d9affe4dd8830
Carson <carsoncolgateccu@gmail.com>
2021-09-16 00:31:10 -0600

    moving on to next confirm prompts

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ var upperCaseli = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 var lowerCaseli = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numli = [0,1,2,3,4,5,6,7,8,9,];
 var specialCharli =  ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^","~", "*", "?", ":"];
-var passlength="";
+var password="";
 var passwordarr= [];
+var passlength= "";
 
 var lowerconfirm=[];
 var upperconfirm=[];
@@ -33,19 +34,19 @@ function random(){
 generateBtn.addEventListener("click", writePassword);
 // Write password to the #password input
 function generatePassword(){
-  passlength = prompt("How many Characters would you like in your password?");{
+  var passlength =prompt("How many Characters would you like in your password?");{
     if (passLength<8 || passLength >128) {
       alert("password does not match criteria min 8: max: 128");}
     else 
-    lowerconfirm= confirm("Do you want lower case characters?");
-    upperconfirm= confirm("Do you want upper case charracters");
+    upperconfirm= confirm("Do you want lower case characters?");
+    lowerconfirm= confirm("Do you want upper case charracters");
     numconfirm= confirm("Do you want numbers?");
     specialcharconfirm= confirm("Do you want special characters?");
     if(!lowercase && !uppercase && !number && !specialChar){
       alert("Please choose one");
-      criteria();
+      generatePassword();
     }};
-    criteria();
+    generatePassword();
   }
 
 
@@ -59,29 +60,3 @@ function writePassword() {
 
 // generateBtn.addEventListener("click", writePassword);
 }
-
-
-// created all my variables for my functions
-  // var array=[];
-  // var char = "";
-  // // these are still showing never read
-  // var passLength;
-  // var uppercase;
-  // var lowercase;
-  // var number;
-  // var specialchar;
-// created first prompt asking telling how many characters my password will generate
-
-// if confirmed this will add a random number,letter,specialchar to pass
-// stuff();
-// if (lowercase){ char += lowerCaseli};
-// if (uppercase){char += upperCaseli};
-// if (number){char += numli};
-// if (specialchar){char += specialCharli};
-//   // makes the choice random
-// for (i=0;i<passlength;i++){
-//   array[i] = char.charAt(Math.floor(Math.random()* char.length));
-// }
-// // makes pass a string
-// array = array.join(" ");
-// return array;

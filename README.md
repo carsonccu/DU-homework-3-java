cfac0062f33c21af9aa38c149c240b43ec7a7bf8
Carson <carsoncolgateccu@gmail.com>
2021-10-08 14:32:19 -0600

    changed functions still returning undefined

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 In this assignment we were required to use HTML, CSS, and Java in order to create a website that would randomly generate a password for you. With html and Css pages already created for you you needed to incorporate the lessons on functions "if statements" and radomizing a list. After doing so the hope was to create a random password between 8-128 characters that included: uppercase letter, lowercase letter, special characters, and numbers. However not all of those criteria needed to be accepted in order to create the password. This project was the hardest yet as I believe javascript and functions is harder for me to grasp.
 
 
-## credits
+## Credits
 [Readme](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
 
 [Methods](w3schools.com/jsref/jsref_random.asp)
--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upperCaseli = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowerCaseli = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numli = [0,1,2,3,4,5,6,7,8,9,];
-var specialCharli =  ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^","~", "*", "?", ":"];
-var password="";
-var passwordarr= [];
-var passlength= "";
+var upperCaseli = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseli = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numli = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+var specialCharli = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":"];
+var finalpassword = "";
+var passwordarr = [];
+var passlength = "";
 
-var lowerconfirm=[];
-var upperconfirm=[];
-var numconfirm=[];
-var specialcharconfirm=[];
+var lowerconfirm = [];
+var upperconfirm = [];
+var numconfirm = [];
+var specialcharconfirm = [];
 
-var passwordarray = function(){
+var passwordarray = function () {
   if (upperconfirm) {
     passswordarr = passwordarr.concat(upperCaseli);
   }
@@ -27,49 +27,42 @@ var passwordarray = function(){
     passswordarr = passwordarr.concat(specialCharli);
   }
   return passwordarr;
-  };
-  // this code still isn't correct
-function random(){
-  return passwordarray[Math.floor(Math.random() * passwordarray.length)];
 };
-generateBtn.addEventListener("click", writePassword);
-// had to had the generate button up here
+
+function random(array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
 // Main password code creating first a prompt followed by the other criteria that can be confirmed
-function generatePassword(){
-  var pass="";
+function generatePassword() {
+  var pass = "";
   passlength = prompt("How many characters do you want in your password");
-  if (!passlength){
+  if (!passlength) {
     alert("please enter a length")
     return
-  } if (passlength>128 || passlength<8) {
-    pass+= pass.concat(length);
+  } if (passlength > 128 || passlength < 8) {
+    pass += pass.concat(length);
     return;
   }
-    upperconfirm= confirm("Do you want lower case characters?");
-    lowerconfirm= confirm("Do you want upper case charracters");
-    numconfirm= confirm("Do you want numbers?");
-    specialcharconfirm= confirm("Do you want special characters?");
-    if(!lowerconfirm && !upperconfirm && !numconfirm && !specialcharconfirm){
-      alert("Please choose one");
-      return;
-    }
+  upperconfirm = confirm("Do you want lower case characters?");
+  lowerconfirm = confirm("Do you want upper case charracters");
+  numconfirm = confirm("Do you want numbers?");
+  specialcharconfirm = confirm("Do you want special characters?");
+  if (!lowerconfirm && !upperconfirm && !numconfirm && !specialcharconfirm) {
+    alert("Please choose one");
+    return;
+  }
 
-    passwordarr()
-    for (i=0;i<=length;i++){
-      // this code still isn't correct
-      password= password.concat(random(passwordarr));
-    }
-    return password;
-  };
+  passwordarray()
+  for (i = 0; i <= length; i++) {
+    finalpassword = finalpassword.concat(random(passwordarr));
+  }
+  return finalpassword;
+};
 
-    
+
 function writePassword() {
-  var password = generatePassword();
+  var finalpassword = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-
-// generateBtn.addEventListener("click", writePassword);
+  passwordText.value = finalpassword;
 }
-    
+generateBtn.addEventListener("click", writePassword);
